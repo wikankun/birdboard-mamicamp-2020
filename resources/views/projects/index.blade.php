433df9cfbafd12f2122f93ad0ff282c80bdec461
Wikan Kuncara Jati <wikan25@gmail.com>
2020-06-03 08:45:25 +0700

    episode 2

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Birdboard</h1>
+    <ul>
+        @foreach ($projects as $project)
+            <li>{{ $project->title }}</li>
+        @endforeach
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/projects', 'ProjectsController@store');
+
+Route::get('/projects', 'ProjectsController@index');
