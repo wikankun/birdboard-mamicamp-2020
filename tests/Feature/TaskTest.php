780d75106cc2262118271c651555be686790b8f5
Wikan Kuncara Jati <wikan25@gmail.com>
2020-06-05 15:26:18 +0700

    episode 21

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -13,15 +13,26 @@ class TaskTest extends TestCase
 
     public function test_it_belongs_to_a_project()
     {
-       $task = factory(Task::class)->create();
+        $task = factory(Task::class)->create();
 
-       $this->assertInstanceOf(Project::class, $task->project); 
+        $this->assertInstanceOf(Project::class, $task->project); 
     }
 
     public function test_it_has_a_path()
     {
-       $task = factory(Task::class)->create();
+        $task = factory(Task::class)->create();
 
-       $this->assertEquals('/projects/' . $task->project->id . '/tasks/' . $task->id, $task->path());
+        $this->assertEquals('/projects/' . $task->project->id . '/tasks/' . $task->id, $task->path());
+    }
+
+    function test_it_can_be_completed()
+    {
+        $task = factory(Task::class)->create();
+
+        $this->assertFalse($task->completed);
+
+        $task->complete();
+
+        $this->assertTrue($task->fresh()->completed);
     }
 }
