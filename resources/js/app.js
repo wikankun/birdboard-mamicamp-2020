62fef4e7e6fd8e505f12d2327ca11d9de7b601a2
Wikan Kuncara Jati <wikan25@gmail.com>
2020-06-06 21:09:06 +0700

    episode 37

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.component('theme-switcher', require('./components/ThemeSwitcher.vue').default);
+
+const app = new Vue({
+    el: '#app'
+});
